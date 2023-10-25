40592d8a102c817ef38e6bbe2cb4ea2228c616d8
a-shdv <shadaev2001@icloud.com>
2023-10-26 00:40:54 +0400

    Initial commit

---
+++ src/main/java/org/university/deanery/DeaneryApplication.java
@@ -0,0 +1,13 @@
+package org.university.deanery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeaneryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeaneryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/university/deanery/DeaneryApplicationTests.java
@@ -0,0 +1,13 @@
+package org.university.deanery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeaneryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
