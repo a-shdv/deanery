7fd469c385f9827ceb4ed96abda22eec25fd19fd
anton.shadaev <anton.shadaev@nordclan.com>
2023-11-30 17:52:09 +0400

    Add authentication

---
+++ src/main/java/org/university/deanery/config/SecurityConfig.java
@@ -0,0 +1,41 @@
+package org.university.deanery.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig  {
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((requests) ->
+                        requests
+                                .requestMatchers(new AntPathRequestMatcher("/")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/sign-in")).permitAll()
+                                .requestMatchers(new AntPathRequestMatcher("/sign-up")).permitAll()
+                                .anyRequest().authenticated()
+                ).formLogin(
+                        form -> form
+                                .loginPage("/sign-in")
+                                .loginProcessingUrl("/sign-in")
+                                .defaultSuccessUrl("/")
+                                .permitAll()
+                ).logout(
+                        LogoutConfigurer::permitAll
+                );
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
---
+++ src/main/java/org/university/deanery/controllers/DeanController.java
@@ -0,0 +1,24 @@
+package org.university.deanery.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class DeanController {
+    @GetMapping("/sign-in")
+    public String signIn() {
+        return "sign-in";
+    }
+
+    @GetMapping("/sign-up")
+    public String signUp() {
+        return "sign-up";
+    }
+
+    @PostMapping("/sign-up")
+    public String signUp(Model model) {
+        return "sign-up";
+    }
+}
