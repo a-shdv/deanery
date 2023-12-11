552f1c2f55038f80a449b3fd74fd03f28720283f
anton.shadaev <anton.shadaev@nordclan.com>
2023-12-11 14:24:13 +0400

    Add subject mvc

---
+++ src/main/java/org/university/deanery/dtos/SubjectDto.java
@@ -0,0 +1,17 @@
+package org.university.deanery.dtos;
+
+import lombok.Getter;
+import org.university.deanery.models.Group;
+import org.university.deanery.models.Subject;
+import org.university.deanery.models.User;
+
+public record SubjectDto(@Getter String title) {
+
+    public SubjectDto(String title) {
+        this.title = title;
+    }
+
+    public static Subject toSubject(SubjectDto subjectDto) {
+        return new Subject(subjectDto.title);
+    }
+}
---
+++ src/main/java/org/university/deanery/services/SubjectService.java
@@ -0,0 +1,46 @@
+package org.university.deanery.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.university.deanery.dtos.SubjectDto;
+import org.university.deanery.models.Subject;
+import org.university.deanery.repositories.SubjectRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class SubjectService {
+    private final SubjectRepository subjectRepository;
+
+    @Autowired
+    public SubjectService(SubjectRepository subjectRepository) {
+        this.subjectRepository = subjectRepository;
+    }
+
+    public void save(Subject subject) {
+        subjectRepository.save(subject);
+    }
+
+    public List<Subject> findAll() {
+        return subjectRepository.findAll();
+    }
+
+    public Optional<Subject> findSubjectByTitle(String title) {
+        return Optional.ofNullable(subjectRepository.findSubjectByTitle(title)).get();
+    }
+
+    public Optional<Subject> findById(Long id) {
+        return Optional.ofNullable(subjectRepository.findById(id)).get();
+    }
+
+    public void updateById(Long id, SubjectDto subjectDto) {
+        Subject subject = subjectRepository.findById(id).get();
+        subject.setTitle(subjectDto.getTitle());
+        subjectRepository.save(subject);
+    }
+
+    public void deleteById(Long id) {
+        subjectRepository.deleteById(id);
+    }
+}
